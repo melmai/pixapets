d57f6c3ed1168fc072fb6b9863a6840bf2f70dd7
Mel <mel@mel.codes>
2023-05-13 21:27:02 -0700

    adds authentication and pulls in pet list data

--- app.py
+++ app.py
@@ -4,12 +4,13 @@
 # flask --app app.py --debug run
 
 from flask import Flask, render_template
+from petfinder import get_pets
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def viewHomePage():
+def home():
     return render_template('home.html')
 
 
@@ -28,9 +29,11 @@ def viewProfile():
     return render_template('profile.html')
 
 
-@app.route('/search')
-def searchPets():
-    return render_template('search.html')
+@app.route('/pets/<string:pet_type>')
+def searchPets(pet_type):
+    pets = get_pets(pet_type)
+    print(pets)
+    return render_template('pets.html', pet_type=pet_type, pets=pets)
 
 
 if __name__ == '__main__':
---
+++ petfinder.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+# credentials for the petfinder API
+api_key = 'aNCEiHafkArcpvUKdOQS6o9SW2E3ujo0oMoHSP3sYTttQr8l35'
+api_secret = 'DYJ4KkyWMcCHMysIEwDTLRwnMIj8Vm1jLoQB1s1f'
+
+def generate_token():
+    # generate token
+    token = requests.post('https://api.petfinder.com/v2/oauth2/token',
+                          data={'grant_type': 'client_credentials',
+                                'client_id': api_key,
+                                'client_secret': api_secret})
+    # convert to json
+    token = token.json()
+
+    # save token
+    with open('token.json', 'w') as f:
+        json.dump(token, f)
+
+    return token
+
+
+def get_pets(pet_type):
+    # get token
+    # with open('token.json', 'r') as f:
+    #     token = json.load(f)
+        
+    token = generate_token()
+
+    # get pets
+    pets = requests.get('https://api.petfinder.com/v2/animals',
+                        headers={'Authorization': 'Bearer ' + token['access_token']},
+                        params={'type': pet_type, 'limit': 100})
+
+    # convert to json
+    pets = pets.json()
+    pets = pets['animals']
+
+    # save pets
+    with open('pets.json', 'w') as f:
+        json.dump(pets, f)
+
+    return pets
