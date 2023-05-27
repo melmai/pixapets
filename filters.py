5908526869558163c7b31908e3c31b1c3327b565
melmai <mel@mel.codes>
2023-05-26 18:36:49 -0700

    hides form labels and adds placeholders

--- filters.py
+++ filters.py
@@ -1,25 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField, EmailField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional
 
-# class LoginForm(FlaskForm):
-#     email = EmailField('email', validators=[DataRequired(), Email(), Length(min=5, max=50)])
-#     password = PasswordField('password', validators=[DataRequired(), Length(min=5, max=50)])
-#     my_textfield = StringField("TextLabel")
-#     my_submit = SubmitField("SubmitName")
 
 class PetFilter(FlaskForm):
     # type - select
-    type = SelectField('Type', choices=[('dog', 'Dog'), ('cat', 'Cat')])
+    type = SelectField('Type', choices=[('all', 'Type (All)'), ('dog', 'Dog'), ('cat', 'Cat')], validators=[Optional()])
 
     # location - text
-    location = StringField("Location")
+    location = StringField("Location", render_kw={"placeholder": "Location"})
 
     # breed - select
-    breed = SelectField('Breed', choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')])
+    # TODO: Generate list of breeds from API
+    breed = SelectField('Breed', choices=[('all', 'Breeds (All)'), ('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')], validators=[Optional()], render_kw={"placeholder": "Type"})
 
     # age - select
-    age = SelectField('Age', choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')])
+    age = SelectField('Age', choices=[('all', 'Age (All)'), ('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')], validators=[Optional()])
 
     # submit - button
     search_bttn = SubmitField("Search")
