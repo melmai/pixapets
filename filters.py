705d0d19560b0e6ddf3d97a4c87e6eab59e67923
melmai <mel@mel.codes>
2023-05-26 18:08:05 -0700

    adds filter fields

--- filters.py
+++ filters.py
@@ -2,10 +2,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField, EmailField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
-class LoginForm(FlaskForm):
-    email = EmailField('email', validators=[DataRequired(), Email(), Length(min=5, max=50)])
-    password = PasswordField('password', validators=[DataRequired(), Length(min=5, max=50)])
+# class LoginForm(FlaskForm):
+#     email = EmailField('email', validators=[DataRequired(), Email(), Length(min=5, max=50)])
+#     password = PasswordField('password', validators=[DataRequired(), Length(min=5, max=50)])
+#     my_textfield = StringField("TextLabel")
+#     my_submit = SubmitField("SubmitName")
 
 class PetFilter(FlaskForm):
-    my_textfield = StringField("TextLabel")
-    my_submit = SubmitField("SubmitName")
+    # type - select
+    type = SelectField('Type', choices=[('dog', 'Dog'), ('cat', 'Cat')])
+
+    # location - text
+    location = StringField("Location")
+
+    # breed - select
+    breed = SelectField('Breed', choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')])
+
+    # age - select
+    age = SelectField('Age', choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')])
+
+    # submit - button
+    search_bttn = SubmitField("Search")
