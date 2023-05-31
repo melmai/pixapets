66676eb9bedef1ffd5e870106a37db1da741b44c
William Chen <wc1321@uw.edu>
2023-05-30 23:38:38 -0700

    initial db made, working on form submitting into db

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 
 # This command will run the app on localhost:5000 and will allow you to see the refreshed app in your browser
 # flask --app app.py --debug run
-
-from flask import Flask, render_template
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, session, request, redirect ,url_for 
 from petfinder import get_pets, get_pet
 from filters import PetFilter
 from signup import SignUp
@@ -11,6 +12,14 @@ from login import Login
 
 app = Flask(__name__)
 app.secret_key="secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    username = db.Column(db.String(50), unique = True, primary_key = True, nullable = False, index = True)
+    password = db.Column(db.String(50), nullable = False)
 
 
 @app.route('/')
@@ -57,6 +66,8 @@ def viewPetDetails(pet_id):
     return render_template('details.html', pet_id=pet_id, pet=pet)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.config['users.spbpro'] = 'db/users.spbpro'
 
     create_user_table()
