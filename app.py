c339a63a4197efd46ee22bd7e74d638584cd5911
Mel <mel@mel.codes>
2023-05-31 19:54:07 -0700

    adds filtering

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # This command will run the app on localhost:5000 and will allow you to see the refreshed app in your browser
 # flask --app app.py --debug run
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from petfinder import get_pets, get_pet, get_breeds
 from filters import PetFilter
 from signup import SignUp
@@ -33,12 +33,19 @@ def viewProfile():
     return render_template('profile.html')
 
 
-@app.route('/pets/<string:pet_type>')
-def searchPets(pet_type):
-    pets = get_pets(pet_type)
+@app.route('/pets/<string:pet_type>', methods=['GET', 'POST'])
+def search_pets(pet_type):
     breeds = get_breeds(pet_type)
     filter = PetFilter()
     filter.breed.choices = filter.breed.choices + breeds
+
+    if request.method == 'POST':
+        breed = filter.breed.data.lower()
+        pets = get_pets(pet_type, location=filter.location.data, distance=filter.distance.data, breed=breed, age=filter.age.data)
+        print(pets)
+    else:
+        pets = get_pets(pet_type)
+        
     return render_template('pets.html', pet_type=pet_type, pets=pets, filter=filter)
 
 @app.route('/pet/<int:pet_id>')
@@ -47,5 +54,5 @@ def viewPetDetails(pet_id):
     print(pet)
     return render_template('details.html', pet_id=pet_id, pet=pet)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug='true', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug='true', port=8000)
--- petfinder.py
+++ petfinder.py
@@ -31,19 +31,32 @@ def get_token():
     return token
 
 
-def get_pets(pet_type):        
-    token = get_token()
+def get_pets(pet_type, **kwargs):        
+    token = generate_token()
+    location = kwargs.get('location', 98404)
+    distance = int(kwargs.get('distance', 100))
+    breed = kwargs.get('breed', 'all')
+    age = kwargs.get('age', 'all')
+
+    request_url = f'https://api.petfinder.com/v2/animals?type={pet_type}'
+    if location:
+        request_url += f'&location={location}&distance={distance}'
+    if breed != 'all':
+        request_url += f'&breed={breed}'
+    if age != 'all':
+        request_url += f'&age={age}'
 
     # get pets
-    pets = requests.get(f'https://api.petfinder.com/v2/animals?type={pet_type}',
-                            headers={'Authorization': 'Bearer ' + token['access_token']})
+    pets = requests.get(request_url, headers={'Authorization': 'Bearer ' + token['access_token']})
+    pets = pets.json()['animals']
+    print(pets)
 
     # convert to json
-    return pets.json()['animals']
+    return pets
 
 
 def get_pet(pet_id):
-    token = get_token()
+    token = generate_token()
 
     # get pets
     pet = requests.get(f'https://api.petfinder.com/v2/animals/{pet_id}',
@@ -53,7 +66,7 @@ def get_pet(pet_id):
     return pet.json()['animal']
 
 def get_breeds(pet_type):
-    token = get_token()
+    token = generate_token()
 
     # get pets
     data = requests.get(f'https://api.petfinder.com/v2/types/{pet_type}/breeds',
@@ -61,6 +74,7 @@ def get_breeds(pet_type):
 
     # convert to json
     data = data.json()['breeds']
+    # print(data)
 
     breeds = [("all", "All Breeds")]
     breeds += [(breed['name'], breed['name']) for breed in data]
